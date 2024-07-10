beac7de6cb357be78dd7211d1db6af676b9dc981
Vlad <vladskocelyas@gmail.com>
2024-07-10 14:09:56 +0300

    new commit

---
+++ views/config.py
@@ -0,0 +1,7 @@
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = 'vladskocelyas@gmail.com'
+MAIL_PASSWORD = 'snbhqrprntalamdu'
+# MAIL_DEFAULT_SENDER = 'noreply@gmail.com'
